61f1531c9981329c61e2a288c710cb9a16a389da
Skh121 <107852103+Skh121@users.noreply.github.com>
2025-06-13 17:59:02 +0545

    Minor Changes

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import LoginPage from "./pages/LoginPage"
+import SignupPage from "./pages/SignupPage"
+import AppRouter from "./routers/AppRouter"
+function App() {
+
+  return (
+    <><AppRouter/></>
+  )
+}
+
+export default App
